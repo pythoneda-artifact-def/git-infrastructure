48d0b3440dfe2458061b95589c0e01186e47658e
rydnr <github@acm-sl.org>
2024-12-11 13:34:11 +0100

    Fixed pythonpackage typo in pyproject.py

--- flake.nix
+++ flake.nix
@@ -134,7 +134,7 @@
               authors = builtins.concatStringsSep ","
                 (map (item: ''"${item}"'') maintainers);
               desc = description;
-              inherit homepage pname pythonMajorMinorVersion pythonpackage
+              inherit homepage pname pythonMajorMinorVersion package
                 version;
               package = builtins.replaceStrings [ "." ] [ "/" ] pythonpackage;
               dbusNext = python.pkgs.dbus-next.version;
